3f2f311b6d978c397f74621fa50c5653b4427f12
Quinn Caverly <quinncaverly@gmail.com>
2024-04-14 15:49:51 -0400

    general style done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Rustacean from "./Rustacean";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Rustacean />
+        <h1>Learning the Rust Programming Language</h1>
+        <h2 className="Sub-title">by Quinn Caverly</h2>
       </header>
+      <div className="App-body">
+        <nav className="TableOfContents">
+          <ul>
+            <li>
+              <a href="#section1">Why Rust?</a>
+            </li>
+            <li className="indented">
+              <a href="#section1">Section 1</a>
+            </li>
+            <li className="indented">
+              <a href="#section2">Section 2</a>
+            </li>
+            <li className="indented">
+              <a href="#section3">Section 3</a>
+            </li>
+            <li className="indented">
+              <a href="#section4">Section 4</a>
+            </li>
+          </ul>
+        </nav>
+        <main className="Content">
+          <section id="abstract">
+            <h2>Why Rust?</h2>
+            <p>
+              Rust is a programming language which is increasing in popularity
+              due to its safety advantages over industry standard languages like
+              C and C++. In fact, via a White House report regarding software
+              safety and security, the US Government recommends using Rust as an
+              alternative to C and C++ in situations where software security is
+              imperative. An increase in usage of the Rust language suggests a
+              potentially financially lucrative situation for those who can
+              effectively write code in Rust. This guide will put forward and
+              describe several resources which can help a coder become
+              comfortable in the Rust programming language. Resources which are
+              helpful for gaining an introduction to Rust will be marked in
+              green and resources for intermediate users will be marked in
+              purple on the table of contents.
+            </p>
+          </section>
+          <section id="section1">
+            <h2>Section 1</h2>
+            <p>Content for Section 1...</p>
+          </section>
+          <section id="section2">
+            <h2>Section 2</h2>
+            <p>Content for Section 2...</p>
+          </section>
+          <section id="section3">
+            <h2>Section 3</h2>
+            <p>Content for Section 3...</p>
+          </section>
+          <section id="section4">
+            <h2>Section 4</h2>
+            <p>Content for Section 4...</p>
+          </section>
+        </main>
+      </div>{" "}
     </div>
   );
 }
